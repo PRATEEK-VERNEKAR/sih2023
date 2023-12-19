9fcc9ddeffbd96a3501132c8a57b8b6fbce46e6a
Prateek Vernekar <mprateekvernekar189@gmail.com>
2023-12-19 16:32:29 +0530

    HI

--- src/app/api/callMLModel/route.js
+++ src/app/api/callMLModel/route.js
@@ -15,50 +15,61 @@ export async function GET(req,res){
         const allMonitorRegions=await MonitorModel.find({});
         const allRegisteredRegions=await Border.find({});
         console.log(allRegisteredRegions);
-        await disconnect()
+        // await disconnect()
+
+        const alertRegionID=[];
 
         for(const singleRegion of allMonitorRegions){
             const imageData=singleRegion['imageData'];
 
             console.log("For region ",singleRegion.regionID);
 
+            const singleRegisteredRegion=allRegisteredRegions.filter((item)=>{return item.regionID===singleRegion.regionID});
+            const tempThreshold=singleRegisteredRegion[0].threshold;
+            console.log(tempThreshold);
+
+
             for(const singleImageData of imageData){
-                const modelPrediction=await axios.post("http://localhost:8080/predict",{
-                    image:singleImageData.image.data
-                });
+                if(!singleImageData.predicted){
 
-                console.log("\t\tImage ID :- ",singleImageData._id);
+                    const modelPrediction=await axios.post("http://localhost:8080/predict",{
+                        image:singleImageData.image.data
+                    });
+                    
+                    console.log("\t\tImage ID :- ",singleImageData._id);
+                    
+                    const countMap = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
+                    
+                    modelPrediction.data.classes.forEach((num)=>{
+                        if (countMap.hasOwnProperty(num)) {
+                            countMap[num]++
+                        }
+                    })
 
-                const countMap = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
-
-                modelPrediction.data.classes.forEach((num)=>{
-                    if (countMap.hasOwnProperty(num)) {
-                        countMap[num]++
+                    console.log(countMap);
+                    
+                    const countMapArray = Object.values(countMap)
+                    
+                    console.log("countMapArray :- ",countMapArray)
+                
+                    const updatedImageData=await MonitorModel.updateOne(
+                        {"_id":singleRegion._id,"imageData._id":singleImageData._id},
+                        {$set:{"imageData.$.classes":countMapArray,"imageData.$.predicted":true}}    
+                    )
+                    
+                    console.log("updatedImageData :- ",updatedImageData);
+                
+                    for(let i=0;i<6;i++){
+                        if(countMap[i]>tempThreshold[i]){
+                            alertRegionID.push(singleRegion.regionID);
+                        }
                     }
-                })
-
-                console.log(countMap);
-
-                const countMapArray = Object.values(countMap)
-                
-                console.log("countMapArray :- ",countMapArray)
-
-                const updatedImageData=await MonitorModel.updateOne(
-                    {"_id":singleRegion._id,"imageData._id":singleImageData._id},
-                    {$set:{"imageData.$.classes":countMapArray,"imageData.$.predicted":true}}    
-                )
-
-                
-                
-
-                console.log("updatedImageData :- ",updatedImageData);
-
+                    
+                    console.log(alertRegionID)
+                }
                 // await delay(10000);
             }
         }
-
-
-        
 
 
         return NextResponse.json({"message":"Objects Predicted Successfully"});

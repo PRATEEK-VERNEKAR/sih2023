c2014e0b6d890232c0f7fcb3a3b505255265fb22
Sadath Syed <sadathsadu2002@gmail.com>
2023-12-19 08:50:02 +0530

    nav

--- src/app/admin/addUser/page.tsx
+++ src/app/admin/addUser/page.tsx
@@ -1,7 +1,7 @@
 // UserRegistration.js
 "use client";
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import axios from 'axios';
 
 export default function UserRegistration() {

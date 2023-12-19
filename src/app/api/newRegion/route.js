10783334d8734387a943f4f8343b5a7082f2c855
Sadath Syed <sadathsadu2002@gmail.com>
2023-12-19 10:49:58 +0530

    thershold backend

--- src/app/api/newRegion/route.js
+++ src/app/api/newRegion/route.js
@@ -5,9 +5,10 @@ import { NextResponse } from "next/server";
 export async function POST(req,res){
     try{
         await connect();
-        const {regionID,name,states,neighborCountry,area,borderLength,govtBodies}=await req.json();
+        const {regionID,name,states,neighborCountry,area,borderLength,govtBodies,roads,buildings,aircrafts,vehicles}=await req.json();
+        const thershold = [buildings,0,roads,vehicles,aircrafts]
         
-        const newRegion=new Border({regionID,name,states,neighborCountry,area,borderLength,govtBodies});
+        const newRegion=new Border({regionID,name,states,neighborCountry,area,borderLength,govtBodies,thershold});
 
         console.log(govtBodies);
         // console.log("\n\nINDIA\n\n");

cc0ec89ee38b4792fb8a10763b69f23ad0d977b8
Sadath Syed <sadathsadu2002@gmail.com>
2023-12-20 04:07:32 +0530

    graphs

---
+++ src/app/graphs/page.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+import LineGraph from '@/components/graphs/LineGraph'
+
+const Graphs = () => {
+  return (
+    <div className='bg-white px-5 py-2'>
+      {/* <h1>this is graph</h1> */}
+        <LineGraph />
+    </div>
+  )
+}
+
+export default Graphs

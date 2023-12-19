da1491d6d3b30d4f774c732796bbaf15e45902d3
Sadath Syed <sadathsadu2002@gmail.com>
2023-12-19 11:05:48 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+'use client'
 import Image from 'next/image'
 import 'animate.css'
+import { useEffect, useState } from 'react'
+import { USER_TOKEN } from '@/utils/consts'
 
 export default function Home() {
+  const [token, setToken] = useState('')
+
+  useEffect(() => {
+    if (USER_TOKEN) {
+      setToken(USER_TOKEN)
+    }
+  }, [])
+
   return (
     <div className="bg-transparent flex flex-row items-center justify-center">
       <a
-        href="/login"
+        href={token ? '/user/allocated_regions' : '/login'}
         className="explore flex flex-row items-center gap-x-3 animate__animated animate__fadeIn"
       >
         <div>Explore</div>

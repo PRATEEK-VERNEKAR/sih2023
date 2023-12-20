61b9bfd0f1217a687b009a5660fafc01fca78d93
Sadath Syed <sadathsadu2002@gmail.com>
2023-12-20 16:49:51 +0530

    changes

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -3,4 +3,4 @@ import Cookies from "js-cookie";
 export const USER_TOKEN = Cookies.get('userToken') ? Cookies.get('userToken') : null
 
 // export const DOMAIN = "http://localhost:3000"
-export const DOMAIN = "https://sih2023-prateek-vernekar.vercel.app"
+export const DOMAIN = 'https://sih2023-omega.vercel.app/'
